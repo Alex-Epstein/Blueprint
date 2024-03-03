d17e485d738ef1ce5a2f341f14ba2325da8cb386
Thomas Ha <thomasha.1310@gmail.com>
2024-03-03 11:53:13 -0500

    Separated slope and scatter methods, added more parameters

--- free_fall.py
+++ free_fall.py
@@ -1,21 +1,33 @@
-import pymunk, pygame, sys
+import pymunk, pygame, sys, random
 
-def create_circle(space):
-    body = pymunk.Body(1, 1, pymunk.Body.DYNAMIC)
-    body.position = (400, 0)
-    shape = pymunk.Circle(body, 40)
+BLACK = pygame.Color(0, 0, 0)
+LIGHT_GREY = pygame.Color(200, 200, 200)
+DARK_GREY = pygame.Color(100, 100, 100)
+WHITE = pygame.Color(255, 255, 255)
+
+pygame.init()
+screen = pygame.display.set_mode((800, 800), pygame.RESIZABLE)
+clock = pygame.time.Clock()
+space = pymunk.Space()
+space.gravity = (0, 98.1)
+
+def create_circle(space, mass=10, moment=100, x=400, y=0, radius=40, nudge_x=False):
+    body = pymunk.Body(mass, moment, pymunk.Body.DYNAMIC)
+    body.position = (x, y)
+    shape = pymunk.Circle(body, radius)
     space.add(body, shape)
+    body.apply_force_at_local_point((random.randrange(-100 * mass, 100 * mass), 0))
     return shape
 
 def draw_circles(circles):
     for circle in circles:
         pos_x = int(circle.body.position.x)
         pos_y = int(circle.body.position.y)
-        pygame.draw.circle(screen, (0, 0, 0), (pos_x, pos_y), 40)
+        pygame.draw.circle(screen, DARK_GREY, (pos_x, pos_y), circle.radius)
 
-def create_bound(space):
+def create_bound(space, a_x, a_y, b_x, b_y, radius):
     body = pymunk.Body(0, 0, pymunk.Body.STATIC)
-    shape = pymunk.Segment(body, (100, 600), (700, 600), 20)
+    shape = pymunk.Segment(body, (a_x, a_y), (b_x, b_y), radius)
     space.add(body, shape)
     return shape
 
@@ -25,28 +37,49 @@ def draw_bounds(bounds):
         a_y = int(bound.a.y)
         b_x = int(bound.b.x)
         b_y = int(bound.b.y)
-        pygame.draw.line(screen, (0, 0, 0), (a_x, a_y), (b_x, b_y), 20)
+        pygame.draw.line(screen, BLACK, (a_x, a_y), (b_x, b_y), int(bound.radius))
 
+def slope():
+    circles = []
+    circles.append(create_circle(space, x=700, nudge_x=True))
+    bounds = []
+    bounds.append(create_bound(space, 400, 500, 700, 400, 10))
 
-pygame.init()
-screen = pygame.display.set_mode((800, 800), pygame.RESIZABLE)
-clock = pygame.time.Clock()
-space = pymunk.Space()
-space.gravity = (0, 100)
-circles = []
-circles.append(create_circle(space))
-bounds = []
-bounds.append(create_bound(space))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
+        screen.fill((255, 255, 255))
+        draw_circles(circles)
+        draw_bounds(bounds)
+        space.step(1.0/60.0)
+        pygame.display.update()
+        clock.tick(120)
 
-    screen.fill((255, 255, 255))
-    draw_circles(circles)
-    draw_bounds(bounds)
-    space.step(1.0/60.0)
-    pygame.display.update()
-    clock.tick(120)
+def scatter(num_circles=100):
+    circles = []
+    for i in range(num_circles):
+        circles.append(create_circle(space, 1, 10, random.randint(100, 700), random.randint(0, 100), 5, False))
+    bounds = []
+    bounds.append(create_bound(space, 100, 700, 700, 700, 5))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        screen.fill((255, 255, 255))
+        draw_circles(circles)
+        draw_bounds(bounds)
+        space.step(1.0/60.0)
+        pygame.display.update()
+        clock.tick(120)
+
+def main():
+    scatter(1000)
+
+if __name__ == "__main__":
+    main()
