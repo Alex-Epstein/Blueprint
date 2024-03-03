2dc706b1806faccc29f62cd1d002df653c2944c6
Alex <zalexep1@gmail.com>
2024-03-03 16:55:41 -0500

    added file

---
+++ calculate.py
@@ -0,0 +1,20 @@
+def calc_free_fall(var):
+    pass
+
+def calc_proj_mot(var):
+    pass
+
+def calc_pend(var):
+    pass
+
+def calc_orbit(var):
+    pass
+
+if type == "Free fall":
+    calc_free_fall()
+elif type == "Projectile motion":
+    calc_proj_mot()
+elif type == "Pendulum":
+    calc_pend()
+else:
+    calc_orbit()
