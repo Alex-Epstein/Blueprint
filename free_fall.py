397504faef06b893e2519909f46ce65e51ee27e4
Thomas Ha <thomasha.1310@gmail.com>
2024-03-03 11:23:52 -0500

    Added bounds to free_fall.py

--- free_fall.py
+++ free_fall.py
@@ -1,24 +1,52 @@
 import pymunk, pygame, sys
 
-BLACK = pygame.Color(0, 0, 0)
-
 def create_circle(space):
-    body = pymunk.Body()
+    body = pymunk.Body(1, 1, pymunk.Body.DYNAMIC)
     body.position = (400, 0)
-    shape = pymunk.Circle(body, 10)
+    shape = pymunk.Circle(body, 40)
     space.add(body, shape)
     return shape
 
 def draw_circles(circles):
     for circle in circles:
-        pos_x = circle.body.position.x
-        pos_y = circle.body.position.y
-        pygame.draw.circle(screen, BLACK, (pos_x, pos_y), 10)
+        pos_x = int(circle.body.position.x)
+        pos_y = int(circle.body.position.y)
+        pygame.draw.circle(screen, (0, 0, 0), (pos_x, pos_y), 40)
+
+def create_bound(space):
+    body = pymunk.Body(0, 0, pymunk.Body.STATIC)
+    shape = pymunk.Segment(body, (100, 600), (700, 600), 20)
+    space.add(body, shape)
+    return shape
+
+def draw_bounds(bounds):
+    for bound in bounds:
+        a_x = int(bound.a.x)
+        a_y = int(bound.a.y)
+        b_x = int(bound.b.x)
+        b_y = int(bound.b.y)
+        pygame.draw.line(screen, (0, 0, 0), (a_x, a_y), (b_x, b_y), 20)
+
 
 pygame.init()
-screen = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
+screen = pygame.display.set_mode((800, 800), pygame.RESIZABLE)
 clock = pygame.time.Clock()
 space = pymunk.Space()
-space.gravity = (0, 9.81)
+space.gravity = (0, 100)
 circles = []
 circles.append(create_circle(space))
+bounds = []
+bounds.append(create_bound(space))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((255, 255, 255))
+    draw_circles(circles)
+    draw_bounds(bounds)
+    space.step(1.0/60.0)
+    pygame.display.update()
+    clock.tick(120)
