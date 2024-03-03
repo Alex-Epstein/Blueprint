e1aeec69a2eff3f340cc00608fbadd0102be803c
Alex <zalexep1@gmail.com>
2024-03-03 14:40:32 -0500

    push

--- pendulum.py
+++ pendulum.py
@@ -1,35 +1,73 @@
-import pygame, pymunk
+import pygame
+import pymunk
 import pymunk.pygame_util
+import math
+
 
 pygame.init()
+
+area = pygame.display.set_mode().get_rect() # Default to screen resolution
+WIDTH, HEIGHT = area[2], area[3]
 window = pygame.display.set_mode((0, 0), pygame.RESIZABLE)
 
 def draw(space, window, draw_options):
-    window.fill("white")
-    space.debug_draw(draw_options)
-    pygame.display.update()
+	window.fill("white")
+	space.debug_draw(draw_options)
+	pygame.display.update()
 
-def run(window):
-    run = True
-    fps = 60
-    dt = 1 / fps
-    clock = pygame.time.Clock()
 
-    space = pymunk.Space()
-    space.gravity = (0, 981)
+def create_pendulum(space):
+	# values inputted
+	r = 80 #cm  
+	m = 1000 #grams 
+	l = 350 #cm
+	A = 40 #degrees
+	T = -1 #seconds
 
-    draw_options = pymunk.pygame_util.DrawOptions(window)
+	bob_x = (l * math.sin(math.radians(A)))
+	bob_y = (l * math.cos(math.radians(A))) 
 
-    while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                break
-        draw(space, window, draw_options)
-        space.step(dt)
-        clock.tick(fps)
+	print(bob_x, bob_y)
 
-    pygame.quit()
+	rotation_center_body = pymunk.Body(body_type=pymunk.Body.STATIC)
+	rotation_center_body.position = (WIDTH/2, HEIGHT/5)
+
+	body = pymunk.Body()
+	body.position = (WIDTH/2, HEIGHT/5)
+	line = pymunk.Segment(body, (0, 0), (bob_x, bob_y), 5) # draws line relative to body (static point)
+	bob = pymunk.Circle(body, r, (bob_x, bob_y))
+	line.friction = 1
+	bob.friction = 1
+	line.mass = 8
+	bob.mass = 30
+	bob.elasticity = 0.95
+	rotation_center_joint = pymunk.PinJoint(body, rotation_center_body, (0, 0), (0, 0))
+	space.add(bob, line, body, rotation_center_joint)
+
+def run(window, width, height):
+	run = True
+	clock = pygame.time.Clock()
+	fps = 60
+	dt = 1 / fps
+
+	space = pymunk.Space()
+	space.gravity = (0, 981) # cm/s^2 ? 
+
+	create_pendulum(space)
+
+	draw_options = pymunk.pygame_util.DrawOptions(window)
+
+	while run:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+				break
+			
+		draw(space, window, draw_options)
+		space.step(dt)
+		clock.tick(fps)
+
+	pygame.quit()
 
 if __name__ == "__main__":
-    run(window)
+	run(window, WIDTH, HEIGHT)
